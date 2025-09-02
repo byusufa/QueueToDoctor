bcfa77ac0b13ee793fdaa3b0592faa6a23a0fce2
byusufa <jozef12079104293@gmail.com>
2025-09-02 08:48:43 +0500

    LoginDto commit

---
+++ src/main/java/uz/pdp/doctor/dto/LoginDto.java
@@ -0,0 +1,15 @@
+package uz.pdp.doctor.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    String username;
+    String password;
+}
